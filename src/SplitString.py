3ba17d43c8e5e59811fddddb78b567f4c2f7d196
Ariel Wolle <36164850+ArielWolle@users.noreply.github.com>
2022-01-22 15:38:23 -0500

    Basic Implementation

--- src/SplitString.py
+++ src/SplitString.py
@@ -4,12 +4,10 @@ import asyncio
 import base64
 import json
 import pyaudio
-import string
-<<<<<<< HEAD
-from number_parser import parse_number
-=======
 import json
->>>>>>> 0f789e3e42d9a3e028d7539fe64200971529a2b8
+import pyautogui
+import pydirectinput
+from number_parser import parse_number
 
 FRAMES_PER_BUFFER = 3200
 FORMAT = pyaudio.paInt16
@@ -25,11 +23,15 @@ counter = 0
 variable_search = False
 variable_temp = ""
 variable_value = 0
+command_temp = ""
+movement_temp = ""
+currentKeys = []
 
-c = open('src\controls.json')
+c = open("src\controls.json")
 controls = json.load(c)
-normal = [[],[],[]]
-variable = [[],[],[]]
+normal = [[], [], []]
+variable = [[], [], []]
+
 
 def reloadJson():
     global normal
@@ -44,19 +46,7 @@ def reloadJson():
         variable[2].append(controls["variable"][i]["movement"])
 
 
-
-def openStream():
-    global stream
-    stream = p.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        frames_per_buffer=FRAMES_PER_BUFFER,
-    )
-
-
-def openStream(index):
+def openStream(index=p.get_default_input_device_info()["index"]):
     global stream
     stream = p.open(
         format=FORMAT,
@@ -82,7 +72,34 @@ def getAudioList():
     return names
 
 
-openStream(0)
+openStream()
+
+
+def stopMovement():
+    global currentKeys
+    for i in currentKeys:
+        pydirectinput.keyUp(i)
+    currentKeys.clear()
+
+
+def move(command, value=None, movement=None, keys=None):
+    global currentKeys
+    if value:
+        if movement == "right":
+            print("MOVING MOUSE")
+            pyautogui.move(int((146 / 90) * value), 0, 0.15)
+        elif movement == "left":
+            pyautogui.move(int(-(146 / 90) * value), 0, 0.15)
+        elif movement == "up":
+            pyautogui.move(0, int((146 / 90) * value), 0.15)
+        elif movement == "down":
+            pyautogui.move(0, int(-(146 / 90) * value), 0, 0.15)
+    elif command == "stop":
+        stopMovement()
+    else:
+        for i in keys:
+            pydirectinput.keyDown(i)
+            currentKeys.append(i)
 
 
 async def send_receive():
@@ -127,6 +144,8 @@ async def send_receive():
             global variable_value
             global normal
             global variable
+            global command_temp
+            global movement_temp
             reloadJson()
             while True:
                 try:
@@ -143,54 +162,63 @@ async def send_receive():
                         words = subSentence.split()
                         # for each word in subsentance
                         for word in words:
-                        # if variable search
+                            # if variable search
                             if variable_search:
-                                parse_value = parse_number(variable_temp + word)
+                                print("parsing :" + variable_temp + " " + word)
+                                parse_value = parse_number(variable_temp + " " + word)
                                 if parse_value:
                                     if parse_value >= 360:
                                         variable_value = 360
                                         variable_search = False
                                     else:
-                                        variable_temp += word
+                                        print("Adding " + word + " to " + variable_temp)
+                                        variable_temp += " " + word
                                 else:
-                                    variable_value = parse_number(variable_temp) if parse_number(variable_temp) else 0
+                                    variable_value = (
+                                        parse_number(variable_temp)
+                                        if parse_number(variable_temp)
+                                        else 0
+                                    )
+                                    print(
+                                        "Search ended final value is "
+                                        + str(variable_value)
+                                    )
+                                    print(
+                                        "Calling move("
+                                        + command_temp
+                                        + ","
+                                        + str(variable_value)
+                                        + ")"
+                                    )
+                                    move(
+                                        command_temp,
+                                        value=variable_value,
+                                        movement=movement_temp,
+                                    )
+                                    variable_temp = ""
+                                    command_temp = ""
+                                    movement_temp = ""
                                     variable_search = False
 
                             # IF WORD IS COMMAND and variable is not true
                             else:
-                                if variable_value != 0:
-                                    print(variable_value)
-                                    variable_value = 0
-                                elif word in normal[0]:
+                                if word in normal[0]:
                                     index = normal[0].index(word)
+                                    move(normal[0][index], keys=normal[1][index])
+                                    # make function call saying move(normal[1][index])
                                     print(normal[1][index])
                                 elif word in variable[0]:
                                     index = variable[0].index(word)
-                                    #print(variable[2][index])
+                                    print(variable[2][index])
+                                    movement_temp = variable[2][index]
                                     variable_search = True
-                                #if variable_value != 0:
-                                    #print(variable_value)
-
-                                    #print(word)
-                                #elif word in controls["variable"]["name"]:
-                                    #print(word)
-                                    #variable_search = True
-                                    # CALL FUNCTION TO DO STUFF
-                                
-                                
-                            # Else wait for next word until number
-                        
-                        # variable search = true
-                        # CALL function with variabnle input
+                                    command_temp = word
                         counter = len(sentance)
 
                 except websockets.exceptions.ConnectionClosedError as e:
                     print(e)
                     assert e.code == 4008
                     break
-                except Exception as e:
-                    print(e)
-                    assert False, "Not a websocket 4008 error"
 
         send_result, receive_result = await asyncio.gather(send(), receive())
 
